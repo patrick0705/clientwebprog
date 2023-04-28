31b34cb6f073fe11a2d91dc851afa3873e4dede1
patrick0705 <patrickschwarzmann87@gmail.com>
2023-04-28 18:21:00 +0200

    bugfix

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const form = document.querySelector('#myForm');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    const formData = new FormData(form);
-    fetch('https://webprogdb29930.azurewebsites.net/api/HttpTrigger1?name='+formData.get('name')+'&email='+formData.get('email'), {
-        method: 'POST',
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    fetch(`https://webprogdb29930.azurewebsites.net/api/HttpTrigger1?name=${name}&email=${email}`, {
+        method: 'POST'
     })
     .then(response => response.text())
     .then(data => {
         console.log(data);
-        form.reset(); // Reset the form fields
+        form.reset(); 
     })
     .catch(error => console.error(error));
-    console.log(formData);
 });
