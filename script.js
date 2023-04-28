30d720607a441f48ad42d104090cc51e6412ebfa
patrick0705 <patrickschwarzmann87@gmail.com>
2023-04-28 18:03:05 +0200

    Website eingefügt

---
+++ script.js
@@ -0,0 +1,12 @@
+const form = document.querySelector('#myForm');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    fetch(`https://webprogdb29930.azurewebsites.net/api/HttpTrigger1?name=${name}&email=${email}`, {
+        method: 'POST'
+    })
+    .then(response => response.text())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+});
